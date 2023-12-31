797149e9ec937fddc173a931278d0bd7941c02c8
kaps02 <kapil02rahate@gmail.com>
2023-12-31 15:21:02 +0530

    process to add vs code to git

---
+++ vsctogit.js
@@ -0,0 +1,7 @@
+console.log("process");
+// git config --global user.name "kaps02"               
+// git config --global user.email "kapil02rahate@gmail.com"  
+// git init
+// git remote add origin https://github.com/kaps02/projects.git
+// git branch -M main
+// git push -u origin main
